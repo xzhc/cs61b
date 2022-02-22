35f54b0b96224433056eee3561846a514d3a370a
xzhc <1838998336@qq.com>
2022-02-22 22:33:28 +0800

    first commit

---
+++ hw0/CE1b.java
@@ -0,0 +1,37 @@
+public class CE1b {
+    public static void main(String[] args) {
+        /*for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        /*for (int i = 1; i <=5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        drawTriangle(5);
+    }
+    public static void drawTriangle(int N) {
+        for (int i = 1; i <=N; i++) {
+            for (int j = N; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
