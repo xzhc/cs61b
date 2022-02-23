19de02378816a19a163d96d254c4c138e9084287
xzhc <1838998336@qq.com>
2022-02-23 18:47:10 +0800

    顺序调换

--- hw0/E2.java
+++ hw0/E2.java
@@ -1,16 +1,21 @@
 public class E2 {
-    public static int max(int[] m){
+    public static int max(int[] m) {
         int current_max = m[0];
-        for (int i = 0; i < m.length; i++) {
+        int i = 0;
+        while (i < m.length) {
             if (current_max<=m[i]) {
                 current_max = m[i];
             }
+            i++;
         }
+        
         return current_max;
     }
-    public static void main(String[] args) {
-        int max_val;
-        int[] numbers = new int[]{9,2,15,12,2,22,10,6};
+
+
+     public static void main(String[] args) {
+        int max_val ;
+        int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
         max_val = max(numbers);
         System.out.println(max_val);
     }
---
+++ hw0/E3.java
@@ -0,0 +1,17 @@
+public class E3 {
+    public static int max(int[] m){
+        int current_max = m[0];
+        for (int i = 0; i < m.length; i++) {
+            if (current_max<=m[i]) {
+                current_max = m[i];
+            }
+        }
+        return current_max;
+    }
+    public static void main(String[] args) {
+        int max_val;
+        int[] numbers = new int[]{9,2,15,12,2,22,10,6};
+        max_val = max(numbers);
+        System.out.println(max_val);
+    }
+}
