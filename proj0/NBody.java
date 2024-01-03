0b5c31adc017443707126e3802797171351aff01
xzhc <1838998336@qq.com>
2024-01-03 16:55:07 +0800

    ReadPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,4 +10,22 @@ public class NBody {
         double radius = in.readDouble();
         return  radius;
     }
+
+    /** ReadPlanets*/
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planets = new Planet[num];
+        for (int i = 0; i < num; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return planets;
+    }
 }
