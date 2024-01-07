cd5e56976c897f2c1dfb744a56ce4b9219edc8f0
xzhc <1838998336@qq.com>
2024-01-07 15:21:21 +0800

    fix two bugs

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -7,13 +7,8 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        /** The origin max function has a bug, now rewrite to implemwnt its function */
+        return a > b ? a : b;
     }
 
 
@@ -35,6 +30,7 @@ public class DebugExercise2 {
             xor = temp;
         }
         return xor;
+
     }
 
     /** Returns a new array where entry i is the max of
@@ -60,7 +56,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
